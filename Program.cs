f74bfa5ff818c8d97ad66a1ecc7f7e1e39743144
Rogério Santos <rogersanctus@gmail.com>
2024-05-26 13:44:45 -0300

    Finish the challange

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia("551198761234", "5200", "1234567890", 2);
+Smartphone iphone = new Iphone("5521998874321", "11", "32112345665", 128);
+
+Console.WriteLine("Smatphone Nokia:");
+nokia.Ligar();
+nokia.InstalarAplicativo("OApp");
+nokia.InstalarAplicativo("MiniSmallApp");
+nokia.ReceberLigacao();
+
+Console.WriteLine("---");
+
+Console.WriteLine("Smatphone Iphone:");
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("All-Node_Modules");
+
